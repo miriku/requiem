d15a738a9976246ebb6da03f0ec45f143df31f67
Edgar Milik <emilik@ucsd.edu>
2015-05-06 22:14:11 -0700

    a test version of a note iterator created. VERY sub par. drop approach and go again

--- midiWriteTest.py
+++ midiWriteTest.py
@@ -1,4 +1,5 @@
 from midiutil.MidiFile import MIDIFile
+import random
 
 def t(measure=0, beats=0, eight=0):
     output = 0.0
@@ -7,6 +8,9 @@ def t(measure=0, beats=0, eight=0):
 
 def add(midi,pitch,time):
     midi.addNote(0,0,pitch,time,1,100)
+
+def printChord(root,offset,note1,note2,note3,note4):
+    print "({}-{}-{}:{}{}{}) ".format(root,offset,note1,note2,note3,note4)
 
 MyMIDI = MIDIFile(1)
 
@@ -28,23 +32,70 @@ pitch = {
             'b5': 71,
         }
 
-# Add a note. addNote expects the following information:
+keyPitch = {
+    0: 52,
+    1: 54,
+    2: 55,
+    3: 57,
+    4: 59,
+    5: 60,
+    6: 62,
+}
 
+offsetPitch = {
+    0: 2,
+    1: 3,
+    2: 5,
+    3: 7,
+    4: 8,
+    5: 10,
+}
+
+notePitch = {
+    0: 64,
+    1: 66,
+    2: 67,
+    3: 69,
+    4: 71,
+    5: 72,
+    6: 74,
+}
+
+# sample usage
+'''
 add(MyMIDI,
     pitch['g5'],
     t(0,0,0))
+    '''
 
-add(MyMIDI,
-    pitch['e5'],
-    t(0,0,0))
+# test case, random generated chords
+for i in range(32):
+    root = random.randint( 0, 6 )
 
-add(MyMIDI,
-    pitch['f#5'],
-    t(1,0,0))
+    offset = offsetPitch[random.randint( 0, 5 )] + keyPitch[root]
 
-add(MyMIDI,
-    pitch['g5'],
-    t(1,0,0.5))
+    note1 = random.randint( 0, 6 )
+
+    note2 = random.randint( 0, 6 )
+    while( note2 == root or note2 == offset ):
+        note2 = random.randint( 0, 6 )
+
+    note3 = random.randint( 0, 6 )
+
+    note4 = random.randint( 0, 6 )
+    while( note4 == note3 ):
+        note4 = random.randint( 0, 6 )
+
+    beat = 0
+
+    printChord( root, offset, note1, note2, note3, note4 )
+    add(MyMIDI, keyPitch[root], t(i,beat,0))
+    add(MyMIDI, keyPitch[root]-24, t(i,beat,0))
+    add(MyMIDI, offset, t(i,beat,0))
+    add(MyMIDI, notePitch[note1], t(i,beat,0))
+    add(MyMIDI, notePitch[note2], t(i,beat+1,0))
+    add(MyMIDI, notePitch[note3], t(i,beat+2,0))
+    add(MyMIDI, notePitch[note4], t(i,beat+3,0))
 
 # And write it to disk.
 binfile = open("output.mid", 'wb')
